64d09d98bfc5452edfe7544f88a583b33db3bae8
elsyanades <elsyana.saltanu@gmail.com>
2022-08-06 14:20:14 +0700

    edit css template

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -9,7 +9,7 @@ $session = \Config\Services::session();
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
         <?= $this->renderSection('title') ?>
         <meta content="Admin Dashboard" name="description" />
-        <meta content="Mannatthemes" name="author" />
+        <meta content="apsm-project" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <link rel="shortcut icon" href="<?=base_url()?>/template/assets/images/favicon.ico">
